d94aedcaf311b3fc6a9cfc25ef7aefec631efaf7
Marta <marta@MacBook-Pro-de-Marta-6.local>
2024-03-16 22:17:21 +0100

    Model available, Preloader, and effect in introduction page

---
+++ loader.js
@@ -0,0 +1,6 @@
+// Preloader
+
+window.onload = function () {
+    document.querySelector("body").removeClass("preload");
+    document.getElementsByClassName("loader-page").style.display = 'none';
+};
---
+++ main.js
@@ -0,0 +1,9 @@
+// Init Particles
+
+window.onload = function() {
+    Particles.init({
+        selector: '.background-banner',
+        color: '#565656',
+        connectParticles: 'true'
+    })
+}
