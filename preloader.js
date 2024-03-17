c4a4e2fae3eb5673ff14f1d44b75f6642ae07230
Marta <marta@MacBook-Pro-de-Marta-6.local>
2024-03-17 10:21:50 +0100

    Adds Preloader when loading the page, with its different script. New logo visible.

---
+++ preloader.js
@@ -0,0 +1,12 @@
+// Preloader
+
+window.addEventListener("load", () => {
+    let loader = document.querySelector(".loader");
+    console.log(loader);
+
+    loader.classList.add("loader-hidden");
+    
+    loader.addEventListener("transitionend", () => {
+        document.body.removeChild("loader");
+    });
+})
