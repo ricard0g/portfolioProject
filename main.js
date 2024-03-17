3915df6555576056dea892b15e0d21a96a00fb2a
Marta <marta@MacBook-Pro-de-Marta-6.local>
2024-03-17 13:07:04 +0100

    Responsiveness of banner almost finished

--- main.js
+++ main.js
@@ -1,9 +1,29 @@
 // Init Particles
 
 window.onload = function() {
+
+    // Normal options Desktop
     Particles.init({
         selector: '.background-banner',
+        maxParticles: 200,
         color: '#565656',
-        connectParticles: 'true'
-    })
+        connectParticles: true,
+        
+        // Responsive Options
+        responsive: [
+            {
+                breakpoint: 1024,
+                options: {
+                    maxParticles: 150
+                }
+            },
+            {
+                breakpoint: 768,
+                options: {
+                    maxParticles: 120,
+                }
+            }
+        ]
+    });
+
 }
