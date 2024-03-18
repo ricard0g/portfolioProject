5fcbadaf2ec5ecc53f94ccf6a8bd1c253c657108
Ricardo Guzman Duran <ricardoguzman444@gmail.com>
2024-03-18 14:07:37 +0100

    Toggle animation when clicking on button finished. Have to style it and adapt and it's finished. Blurred and semi-transparent look would be cool.

--- main.js
+++ main.js
@@ -1,66 +1,68 @@
-// Toggle animation
+// Toggle animation and then the elements part of the toggle animation
 
 const toggle = document.querySelector(".toggle");
 console.log(toggle);
 
-// A good idea is to create two functions, one when the element hasn't been clicked (animation would run), and another when animation has been clicked (animation returns to normal). Saving if the animation ran or not in a boolean variable like "let clicked = false".
-
-toggle.onclick = () => {
-	let topLine = document.querySelector(".top_line");
+let topLine = document.querySelector(".top_line");
 	let middleLine = document.querySelector(".middle_line");
 	let bottomLine = document.querySelector(".bottom_line");
-	let clicked = true;
+	let sectionSlide = document.querySelector(".links-mobile-tablet");
 
+// A good idea is to create two functions, one when the element hasn't been clicked (animation would run), and another when animation has been clicked (animation returns to normal). Saving if the animation ran or not in a boolean variable like "let clicked = false".
+let animOn = false;
+
+const toggleOn = () => {
+	animOn = true;
+
+	//Slide on
+	sectionSlide.style.right = '2vw';
+	sectionSlide.style.opacity = '1';
 	// Top line rotation
-	topLine.style.left = "2px";
-	topLine.style.top = "14px";
-	topLine.style.width = "25px";
+	topLine.style.left = "14.68px";
+	topLine.style.top = "23px";
+	topLine.style.width = "19px";
 	topLine.style.transform = "rotate(45deg)";
 	// Bottom Line rotation
-	bottomLine.style.left = "2px";
-	bottomLine.style.top = "14px";
-	bottomLine.style.width = "25px";
+	bottomLine.style.left = "14.68px";
+	bottomLine.style.top = "23px";
+	bottomLine.style.width = "19px";
 	bottomLine.style.transform = "rotate(-45deg)";
 	// Middle Line translation in the main axis
 	middleLine.style.opacity = "0";
-	middleLine.style.transform = "translateX(20px)";
+	middleLine.style.transform = "translateX(14px)";
+}
 
-	if (clicked) {
-		toggle.onclick = () => {
-			// Top line rotation
-			topLine.style.left = "50%";
-			topLine.style.top = "30%";
-			topLine.style.width = "20px";
-			topLine.style.transform = "translate(-50%, -50%)";
-			// Bottom Line rotation
-			bottomLine.style.left = "50%";
-			bottomLine.style.top = "70%";
-			bottomLine.style.width = "20px";
-			bottomLine.style.transform = "translate(-50%, -50%)";
-			// Middle Line translation in the main axis
-			middleLine.style.left = "50%";
-			middleLine.style.top = "50%";
-			middleLine.style.opacity = "1";
-			middleLine.style.transform = "translate(-50%, -50%)";
-		};
+const toggleOff = () => {
+	animOn = false;
+
+	// Slide off
+	sectionSlide.style.right = '-40%';
+	sectionSlide.style.opacity = '0';
+	// Top line rotation
+	topLine.style.left = "50%";
+	topLine.style.top = "35%";
+	topLine.style.width = "14px";
+	topLine.style.transform = "translate(-50%, -50%)";
+	// Bottom Line rotation
+	bottomLine.style.left = "50%";
+	bottomLine.style.top = "65%";
+	bottomLine.style.width = "14px";
+	bottomLine.style.transform = "translate(-50%, -50%)";
+	// Middle Line translation in the main axis
+	middleLine.style.left = "50%";
+	middleLine.style.top = "50%";
+	middleLine.style.width = "14px";
+	middleLine.style.opacity = "1";
+	middleLine.style.transform = "translate(-50%, -50%)";
+}
+
+toggle.addEventListener('click', () => {
+	if(animOn){
+		toggleOff();
 	} else {
-		toggle.onclick = () => {
-			// Top line rotation
-			topLine.style.left = "2px";
-			topLine.style.top = "14px";
-			topLine.style.width = "25px";
-			topLine.style.transform = "rotate(45deg)";
-			// Bottom Line rotation
-			bottomLine.style.left = "2px";
-			bottomLine.style.top = "14px";
-			bottomLine.style.width = "25px";
-			bottomLine.style.transform = "rotate(-45deg)";
-			// Middle Line translation in the main axis
-			middleLine.style.opacity = "0";
-			middleLine.style.transform = "translateX(20px)";
-		};
+		toggleOn();
 	}
-};
+})
 
 // Init Particles
 
