1ecc01de0bcb9b5b6cb7f3838eade3a21b08c62a
Ricardo Guzman Duran <ricardoguzman444@gmail.com>
2024-03-18 20:15:42 +0100

    I think that what has to be animated is the anchor tag, not the list item.

--- preloader.js
+++ preloader.js
@@ -1,8 +1,8 @@
 // Preloader and Navbar Links animation
-let li1 = document.querySelector(".li1-desktop");
-console.log(li1);
-let li2 = document.querySelector(".li2-desktop");
-let li3 = document.querySelector(".li3-desktop");
+let li1 = document.getElementById("li1-desktop");
+console.log(typeof li1);
+let li2 = document.getElementById("li2-desktop");
+let li3 = document.getElementById("li3-desktop");
 
 window.addEventListener("load", () => {
 	let loader = document.querySelector(".loader");
