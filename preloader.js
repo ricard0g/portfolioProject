aa240a0611461b77e466d7ccf310e38fd9e7c125
Ricardo Guzman Duran <ricardoguzman444@gmail.com>
2024-03-19 09:08:30 +0100

    Reset commit made but bad chosen

--- preloader.js
+++ preloader.js
@@ -1,12 +1,21 @@
-// Preloader
+// // Preloader and Navbar Links animation
+// let li1 = document.getElementById("li1-desktop");
+// console.log(typeof li1);
+// let li2 = document.getElementById("li2-desktop");
+// let li3 = document.getElementById("li3-desktop");
 
 window.addEventListener("load", () => {
-    let loader = document.querySelector(".loader");
-    console.log(loader);
+	let loader = document.querySelector(".loader");
+	console.log(loader);
 
-    loader.classList.add("loader-hidden");
-    
-    loader.addEventListener("transitionend", () => {
-        document.body.removeChild(loader);
-    });
-})
+	loader.classList.add("loader-hidden");
+
+	loader.addEventListener("transitionend", () => {
+		document.body.removeChild(loader);
+
+		// // Little animation when windows load for the links in navbar in desktop
+		// li1.classList.add("up");
+		// li2.classList.add("up");
+		// li3.classList.add("up");
+	});
+});
